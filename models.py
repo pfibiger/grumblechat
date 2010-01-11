436cf0e015bdf071238be8747e257f263fd98e0b
Jeremy Muhlich <jmuhlich@bitflood.org>
2010-01-11 15:05:07 -0500

    initial commit ---  app.yaml   |    8 ++++++++  index.yaml |   11 +++++++++++  main.py    |   40 ++++++++++++++++++++++++++++++++++++++++  models.py  |   18 ++++++++++++++++++  4 files changed, 77 insertions(+), 0 deletions(-)  create mode 100644 app.yaml  create mode 100644 index.yaml  create mode 100755 main.py  create mode 100755 models.py

---
+++ models.py
@@ -0,0 +1,18 @@
+from google.appengine.ext import db
+
+
+class Account(db.Model):
+    user = db.UserProperty(required=True)
+    nickname = db.StringProperty(required=True)
+
+
+class Room(db.Model):
+    name = db.StringProperty(required=True)
+    topic = db.StringProperty()
+
+
+class Message(db.Model):
+    sender = db.ReferenceProperty(reference_class=Account, required=True)
+    room = db.ReferenceProperty(reference_class=Room, required=True)
+    timestamp = db.DateTimeProperty(auto_now_add=True, required=True)
+    content = db.StringProperty(required=True)
