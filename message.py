ec681b7845cbd8566a5aed3096caa138ec21f6b0
Philip Fibiger <philip@fibiger.org>
2010-02-06 15:00:57 -0500

    added ability to set topic.

--- message.py
+++ message.py
@@ -25,9 +25,25 @@ class MessageCollectionHandler(webapp.RequestHandler):
             message = Message(sender=sender, room=room, timestamp=timestamp, content=content)
             message.put()
         self.redirect('/room/' + room_key)
-            
 
-application = webapp.WSGIApplication([(r'/room/([^/]+)/msg', MessageCollectionHandler)],
+
+class TopicCollectionHandler(webapp.RequestHandler):
+
+    def post(self, room_key):
+        user = users.get_current_user()
+        sender = Account.all().filter('user =', user).get()
+        room = Room.all().filter('__key__ =', Key(room_key)).get()
+        topic = self.request.get('topic')
+        if not sender:
+            # no account for this user
+            self.response.out.write(template.render('templates/account_create.html', None))
+        elif len(topic):
+			room.topic = topic
+			room.put()
+        self.redirect('/room/' + room_key)            
+
+application = webapp.WSGIApplication([(r'/room/([^/]+)/msg', MessageCollectionHandler),
+									   (r'/room/([^/]+)/topic',TopicCollectionHandler)],
                                      debug=True)
 
 def main():
