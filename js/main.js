23b48097cc9fa820ab57b74188a4c4b13badd2e9
Jeremy Muhlich <jmuhlich@bitflood.org>
2010-10-12 00:05:10 -0400

    unified message timestamp processing. now clients see local time for both server-pre-rendered and ajax-added messages. fixes #20

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ var chat = function() {
     var update_interval_error = 1000 * 10;
     var update_interval = update_interval_min;
     var message_display_max = 70;
+    var timestamp_display_format = 'g:i&\\n\\b\\s\\p;A';
+    var timestamp_iso8601_format = 'Y-m-d\TH:i:s';
     var do_polling = true;
 
     var room;
@@ -37,9 +39,28 @@ var chat = function() {
         $('body').scrollTop(dest); // FIXME this is a chromium workaround for bug #2891
     }
 
-    function sendMessage(msg) {
+    /* formats a timestamp abbr content based on an ISO8601 timestamp in its title attr, or a
+    separate date object */
+    function localizeTimestamp($abbr, timestamp) {
+        if (timestamp == null) {
+            timestamp = parseDate($abbr.attr('title'));
+        } else {
+            $abbr.attr('title', timestamp.format(timestamp_iso8601_format));
+        }
+        $abbr.html(timestamp.format(timestamp_display_format));
+    }
+
+    function createMessage(timestamp, sender, msg) {
         var $msg_new = $msg_template.clone();
-        var myDate = new Date();
+        $msg_new.css('display', '');
+        localizeTimestamp($msg_new.find('.msg-timestamp abbr'), timestamp);
+        $msg_new.find('.msg-sender').html(sender);
+        $msg_new.find('.msg-content').html(msg);
+        return $msg_new;
+    }
+
+    function sendMessage(msg) {
+        var $msg_new;
 
         function success(data) {
             do_polling = true;
@@ -53,11 +74,8 @@ var chat = function() {
             do_polling = true;
         }
 
-        $msg_new.css('display', '');
+        $msg_new = createMessage(new Date(), account.nickname, msg);
         $msg_new.attr("id","message-temp-local");
-        $msg_new.find('.msg-timestamp').html(formatTime(myDate));
-        $msg_new.find('.msg-sender').html(account.nickname);
-        $msg_new.find('.msg-content').html(msg);
         $msg_template.before($msg_new);
         scrollToBottom();
         var post_url = '/api/room/' + room.key + '/msg/';
@@ -104,12 +122,9 @@ var chat = function() {
                         $found_message.find('.msg-content').html(message.content);
                     } 
                     else {
-                        var $msg_new = $msg_template.clone();
-                        $msg_new.css('display', '');
+                        var $msg_new = createMessage(parseDate(message.timestamp),
+                                                     message.sender_name, message.content);
                         $msg_new.addClass('event-' + message.event);
-                        $msg_new.find('.msg-timestamp').html(formatTime(parseDate(message.timestamp)));
-                        $msg_new.find('.msg-sender').html(message.sender_name);
-                        $msg_new.find('.msg-content').html(message.content);
                         $msg_template.before($msg_new);
                     }
                 });
@@ -163,18 +178,6 @@ var chat = function() {
         return date;
     }
 
-    function formatTime(date) {
-        var parts = [date.getHours(), date.getMinutes(), date.getSeconds()];
-
-        for (var i=0; i<parts.length; i++) {
-            parts[i] = parts[i].toString();
-            if (parts[i].length == 1)
-                parts[i] = '0' + parts[i];
-        }
-
-        return parts.join(':');
-    }
-
     function initialize(the_room, the_account, message_last_key) {
         // initialize "statics"
         room = the_room;
@@ -195,6 +198,7 @@ var chat = function() {
         $('#text-entry').submit(textEntrySubmit);
 
         // prepare the window for user interaction
+        $('.message:visible .msg-timestamp abbr').each(function () { localizeTimestamp($(this)) });
         scrollToBottom();
         $('#text-entry-content').focus();
 
@@ -209,3 +213,8 @@ var chat = function() {
     };
 
 }();
+
+
+// add .format to Date objects -- emulates PHP's date()
+// from http://jacwright.com/projects/javascript/date_format
+Date.prototype.format=function(format){var returnStr='';var replace=Date.replaceChars;for(var i=0;i<format.length;i++){var curChar=format.charAt(i);if(i-1>=0&&format.charAt(i-1)=="\\"){returnStr+=curChar;}else if(replace[curChar]){returnStr+=replace[curChar].call(this);}else if(curChar!="\\"){returnStr+=curChar;}}return returnStr;};Date.replaceChars={shortMonths:['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],longMonths:['January','February','March','April','May','June','July','August','September','October','November','December'],shortDays:['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],longDays:['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],d:function(){return(this.getDate()<10?'0':'')+this.getDate();},D:function(){return Date.replaceChars.shortDays[this.getDay()];},j:function(){return this.getDate();},l:function(){return Date.replaceChars.longDays[this.getDay()];},N:function(){return this.getDay()+1;},S:function(){return(this.getDate()%10==1&&this.getDate()!=11?'st':(this.getDate()%10==2&&this.getDate()!=12?'nd':(this.getDate()%10==3&&this.getDate()!=13?'rd':'th')));},w:function(){return this.getDay();},z:function(){var d=new Date(this.getFullYear(),0,1);return Math.ceil((this-d)/86400000);},W:function(){var d=new Date(this.getFullYear(),0,1);return Math.ceil((((this-d)/86400000)+d.getDay()+1)/7);},F:function(){return Date.replaceChars.longMonths[this.getMonth()];},m:function(){return(this.getMonth()<9?'0':'')+(this.getMonth()+1);},M:function(){return Date.replaceChars.shortMonths[this.getMonth()];},n:function(){return this.getMonth()+1;},t:function(){var d=new Date();return new Date(d.getFullYear(),d.getMonth(),0).getDate()},L:function(){var year=this.getFullYear();return(year%400==0||(year%100!=0&&year%4==0));},o:function(){var d=new Date(this.valueOf());d.setDate(d.getDate()-((this.getDay()+6)%7)+3);return d.getFullYear();},Y:function(){return this.getFullYear();},y:function(){return(''+this.getFullYear()).substr(2);},a:function(){return this.getHours()<12?'am':'pm';},A:function(){return this.getHours()<12?'AM':'PM';},B:function(){return Math.floor((((this.getUTCHours()+1)%24)+this.getUTCMinutes()/60+this.getUTCSeconds()/3600)*1000/24);},g:function(){return this.getHours()%12||12;},G:function(){return this.getHours();},h:function(){return((this.getHours()%12||12)<10?'0':'')+(this.getHours()%12||12);},H:function(){return(this.getHours()<10?'0':'')+this.getHours();},i:function(){return(this.getMinutes()<10?'0':'')+this.getMinutes();},s:function(){return(this.getSeconds()<10?'0':'')+this.getSeconds();},u:function(){var m=this.getMilliseconds();return(m<10?'00':(m<100?'0':''))+m;},e:function(){return"Not Yet Supported";},I:function(){return"Not Yet Supported";},O:function(){return(-this.getTimezoneOffset()<0?'-':'+')+(Math.abs(this.getTimezoneOffset()/60)<10?'0':'')+(Math.abs(this.getTimezoneOffset()/60))+'00';},P:function(){return(-this.getTimezoneOffset()<0?'-':'+')+(Math.abs(this.getTimezoneOffset()/60)<10?'0':'')+(Math.abs(this.getTimezoneOffset()/60))+':00';},T:function(){var m=this.getMonth();this.setMonth(0);var result=this.toTimeString().replace(/^.+ \(?([^\)]+)\)?$/,'$1');this.setMonth(m);return result;},Z:function(){return-this.getTimezoneOffset()*60;},c:function(){return this.format("Y-m-d\\TH:i:sP");},r:function(){return this.toString();},U:function(){return this.getTime()/1000;}};
