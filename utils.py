d0805ac0b9642877b3077d51286c75c4b85a7c59
Philip Fibiger <philip@fibiger.org>
2011-01-05 10:24:21 -0500

    allow .jpeg for image embedding

--- utils.py
+++ utils.py
@@ -79,7 +79,7 @@ def transform_message(message):
     )
     if content is not None:
         content = re.sub(r"((?:https?)://[^ \t\n\r()\"']+)", r"<\1>", content)
-        content = re.sub(r"<(http[^>]+(?:jpg|png|gif))>", r"[![Image](\1)](\1)", content)
+        content = re.sub(r"<(http[^>]+(?:jpg|jpeg|png|gif))>", r"[![Image](\1)](\1)", content)
         if (Message_event_names[message.event] == "topic"):
             message.content = md_nohtml.convert(content)
             message.content = re.sub("<\/?p>","", message.content)
